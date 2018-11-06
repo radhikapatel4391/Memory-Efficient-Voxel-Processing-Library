31baf6a69a7b9d29147335793eb71d27d9f79582
radhikapatel4391 <radhika.patel4391@gmail.com>
2018-11-06 11:33:53 -0500

    update
    update

--- src/client.py
+++ src/client.py
@@ -5,55 +5,64 @@ import numpy as np
 from scipy import ndimage
 import os
 import psutil
+
+# print(psutil.cpu_percent())
+# print(psutil.virtual_memory())
 process = psutil.Process(os.getpid())
-print("client PID: ",psutil.Process(os.getpid()))
+#print("client PID: ",psutil.Process(os.getpid()))
 #print(process.memory_info().rss)
 
 filename = 'gyroidUniform.npy'   
 structure = np.ones((3,3,3))
-#input = np.load(filename, mmap_mode="r")
-#print(process.memory_info().rss)
+input = np.load(filename, mmap_mode="r")
+
+
+
+
 
 #generating sparse array of different size...
-input = random(40000,200,density=0.5,dtype="float64")
-input = input.todense()
-input = np.array(input)
-input = np.reshape(input,(200,200,200))
+# input = random(40000,200,density=0.5,dtype="float64")
+# input = input.todense()
+# input = np.array(input)
+# input = np.reshape(input,(200,200,200))
 #............
 
 #Test start...............
 
 #...............
-print("Nothing testing....")
+
+print("\nNothing testing....")
 start_time = t.time()
-process = psutil.Process(os.getpid())
 x = int(process.memory_info().rss)
 output = vc.nothing(input)
 y = int(process.memory_info().rss)
-print("------------momory used: ",y-x)
-print("VC--- %s seconds ---" % (t.time() - start_time))
+print("Memory: ",(y-x)/1000000)
+print("sec:    ",(t.time() - start_time))
 print("result: ",(input==output).all())
 
 
 
 #..............
-print("grey_dilation testing...")
+print("\ngrey_dilation VoxelProcessind")
 start_time = t.time()
-process = psutil.Process(os.getpid())
 x = int(process.memory_info().rss)
 output = vc.grey_dilation(input,structure=structure)
 y = int(process.memory_info().rss)
-print("----------------momory used: ",y-x)
-print("VC--- %s seconds ---" % (t.time() - start_time))
+print("Memory: ",(y-x)/1000000)
+print("sec:    ",(t.time() - start_time))
+
+
 #......
+print("\ngrey_dilation Default")
 start_time = t.time()
-process = psutil.Process(os.getpid())
 x = int(process.memory_info().rss)
 d = ndimage.grey_dilation(input,structure=structure)
 y = int(process.memory_info().rss)
-print("----------------momory used: ",y-x)
-print("DE--- %s seconds ---" % (t.time() - start_time))
-print("result: ",(d==output).all())
+print("Memory: ",(y-x)/1000000)
+print("sec:    ",(t.time() - start_time))
+
+print("\nresult: ",(d==output).all())
+
 
 #.....................		
 	
