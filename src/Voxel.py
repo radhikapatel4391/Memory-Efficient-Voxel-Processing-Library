c4c4dc01964de426934dc912eca24400c0548535
radhikapatel4391 <radhika.patel4391@gmail.com>
2018-11-21 01:00:32 -0500

    update

--- src/Voxel.py
+++ src/Voxel.py
@@ -1,12 +1,15 @@
-import voxelProcessing as v
+from voxelProcessing import VoxelProcessing
 
 
 #Doing not any operation ...	
-def nothing(input,blockSize=20,fakeGhost=1,):
-	return v.main(input,blockSize,fakeGhost)
+def nothing(input,blockSize=50,fakeGhost=1):
+	v = VoxelProcessing(input,blockSize,fakeGhost)
+	return v.main()
 	
 #function...
-def grey_dilation(input,blockSize=20,fakeGhost=1,size=None, footprint=None, structure=None, output=None, mode='reflect', cval=0.0, origin=0):
-
-	operationArgumentDic = {"size":size,"footprint":footprint,"structure":structure,"output":output,"mode":mode,"cval":cval,"origin":origin}
-	return v.main(input,blockSize,fakeGhost,operation="dilation",operationArgumentDic=operationArgumentDic)
+def grey_dilation(input,blockSize=50,fakeGhost=None,size=None, footprint=None, structure=None, output=None, mode='reflect', cval=0.0, origin=0):
+	if not fakeGhost:
+		fakeGhost = structure.shape[0]//2
+	operationArgumentDic = {"size":size,"footprint":footprint,"structure":structure,"output":output,"mode":mode,"cval":cval,"origin":origin}	
+	v = VoxelProcessing(input,blockSize,fakeGhost,operation="dilation",operationArgumentDic=operationArgumentDic)
+	return v.main()
